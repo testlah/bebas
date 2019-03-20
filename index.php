c857cbe218b6331d00722277c5fd67dbe474efb6
testbrolah <48765638+testbrolah@users.noreply.github.com>
2019-03-20 23:03:23 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+<title>#LNX_CREW #Ex07</title>>
+   
+   <meta charset="utf-8">
+   <meta content="Ex07" name="description"/>
+   <meta content="Ex07" name="keyword"/>
+  <style type="text/css">
+			body {
+				
+				background-image:url('https://i.ibb.co/Xs2y5q3/images-1-picsay.jpg');
+				background-color: #000000;
+				background-repeat:no-repeat;
+				background-size: 100% ;
+				background-position:top center;
+				
+			}
+			
+		</style>
+     </head>
+         &nbsp;     
+           <center><img src="https://4.bp.blogspot.com/-gyXTeoYfZYU/WLGzd5IbNEI/AAAAAAAAAgI/7yE1wnuMN4ssfET4BA4GIq18k6xTETuEQCLcB/s1600/logo%2B3.png"width="300"height="300"></center></body>
+            
+             <center><font face="Forte" size="9" style="color:#ffffff;text-shadow:#FF0099 0px 0px 10px">#LNX _CREW</font></center>
+               
+               <center><font style="color:#ffffff;text-shadow: 0px 0px 10px blue" size="7">....:~Ex07~:....</font></center>
+               
+           <center><font color="yellow" size="6">- - - - - - - - - - - - - - - - -  </font></center>
+            
+             <center><font face="Stencil" size="5" color="red">[ Your Scurity,<font face="Stencil" size="5" color="green"> Hass Been Exploited ]<br>
+             <font style="color:red;text-shadow:#000 0px 0px 3px">:= My<font style="color:white;text-shadow:#000 0px 0px 3px">Friend's =:<br>
+             <div class="greets">
+<table align=center border="0">
+<tr>
+<td width=100% id=greetz>
+<marquee behavior="scroll" direction="left" scrollamount="4" scrolldelay="55" width="100%">
+<font size="5px" style="font-family: Iceland, sans-serif;color:white;text-shadow: 0 0 3px red, 0px 0px 5px red" >
+<b>-=|-Omest - Marshall - Wahyudi - Holilul Anwar - Vijune15 - Inyour Good - Inyour mrX - Lool 00T - Sarkevih - Tuyul Botak - Yuudachipoi - Lolipop - Death Hinote - 3XP3T4S1 - Extazy Kun-|=-</font>
+</marquee>
+</td>
+</table></div> 
+             
+          <center><font color="yellow" size="6"> - - - - - - - - - - - - - - - - - </font></center>
+          <iframe width="0" height="0" src="https://instaud.io/_/3r3W.mp3" frameborder="0" allowfullscreen></iframe> </center></div></div></content></body></html>
+    <body><marquee width="600">Why Me ?</marquee><br> <marquee scrollamount="50" width="600">extazy-kun.blogspot.com</marquee><br> <marquee scrollamount="50" width="600">extazykun06@gmail.com</marquee> <br> <marquee scrollamount="50" width="600">extazy-kun.blogspot.com</marquee><br><marquee scrollamount="50" width="600">extazykun06@gmail.com</marquee><br><marquee scrollamount="50" width="600">extazy-kun.blogspot.com</marquee><br> <marquee scrollamount="50" width="600">extazykun06@gmail.com</marquee><br><marquee scrollamount="50" width="600">extazy-kun.blogspot.com</marquee><br><marquee scrollamount="50" width="600">extazykun06@gmail.com</marquee><br><marquee scrollamount="50" width="600">extazy-kun.blogspot.com</marquee><br> <marquee scrollamount="50" width="600">extazykun06@gmail.com</marquee><br><marquee scrollamount="50" width="600">extazy-kun.blogspot.com</marquee><br><marquee scrollamount="50" width="600">extazykun06@gmail.com</marquee><br><marquee scrollamount="50" width="600">extazy-kun.blogspot.com</marquee><br> <marquee scrollamount="50" width="600">extazykun06@gmail.com</marquee> <br><marquee scrollamount="50" width="600">extazy-kun.blogspot.com</marquee><br><marquee scrollamount="50" width="600">extazykun06@gmail.com</marquee><br><marquee scrollamount="50" width="600">extazy-kun.blogspot.com</marquee><br> <marquee scrollamount="50" width="600">extazykun06@gmail.com</marquee>
+</body> </html>
